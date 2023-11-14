03084cb8f06181289a61c3df2fe87cb045798c8c
ahmed samir sayed ahmed <105457338+hora459@users.noreply.github.com>
2023-11-14 23:04:36 +0200

    init backend

---
+++ Backend/NutriBalance/src/test/java/com/example/nutribalance/NutriBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutribalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
