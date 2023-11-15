5502812a85b478d5504d93ca439802809e1edee7
mado2002 <mohamedww397@gmail.com>
2023-11-15 21:37:30 +0200

    Database final layout

--- Backend/NutriBalance/src/main/java/com/example/nutribalance/Controllers/CoachController.java
+++ Backend/NutriBalance/src/main/java/com/example/nutribalance/Controllers/CoachController.java
@@ -10,9 +10,5 @@ import org.springframework.web.bind.annotation.*;
 public class CoachController {
     @Autowired
     private  Iservice iservice;
-    @PostMapping("/save")
-    public Coach saveCoachWithUsers(){
-        return iservice.saveCoachWithUsers();
-    }
 
 }
