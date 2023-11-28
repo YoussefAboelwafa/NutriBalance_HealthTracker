ab71d26e560fae2fb889a30d2f3010bdbb987c5b
ayman988 <aymanemad222@gmail.com>
2023-11-29 01:33:57 +0200

    add radio button

---
+++ Frontend/NutriBalance/src/app/role-type/role-type.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-role-type',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './role-type.component.html',
+  styleUrl: './role-type.component.css'
+})
+export class RoleTypeComponent {
+selected_role:string='';
+constructor(private router: Router) {}
+
+ onSelection(){
+   if(this.selected_role=='coach') this.router.navigate(['/coach-signup']);
+}
+}
