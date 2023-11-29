034ecaad04c8f24e10901e3dd193f51007216c39
yousefelmedany <yousefelmedany72@gmail.com>
2023-11-29 08:24:22 +0200

    AdminPage Added

---
+++ Frontend/NutriBalance/src/app/adminpage/adminpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-adminpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './adminpage.component.html',
+  styleUrl: './adminpage.component.css'
+})
+export class AdminpageComponent {
+
+}
