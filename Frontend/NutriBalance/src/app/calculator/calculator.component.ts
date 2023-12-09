2781d1406acf3301ba602a3065e150707df28dc8
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-12-09 19:25:11 +0200

    GDA form completed

--- Frontend/NutriBalance/src/app/calculator/calculator.component.ts
+++ Frontend/NutriBalance/src/app/calculator/calculator.component.ts
@@ -3,21 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  userGDA:any = {
-    weigth: 0,
-    height: 0,
-    age: 0,
-    gender:'',
-    activity:'',
+  userGDA: any = {
+    weight: null,
+    height: null,
+    age: null,
+    gender: 'male',
+    activity: 'low',
+  };
+  calculateGDA() {
+    console.log(this.userGDA);
   }
-
-  ngOnInit(): void {
-  }
-
 }
